6abe12bae21fd8499b3829430568374aae1f9d5e
folkpark <parkerfolkman@gmail.com>
2016-02-13 14:58:44 -0700

    Update bin_packing.py

--- bin_packing.py
+++ bin_packing.py
@@ -37,6 +37,8 @@ def find_my_solution (rectangles):
 
 
 
+
+    '''
     #Fill the array with X's
     for i in range(length): #Length
         tempBox = rectangles[i]
@@ -45,7 +47,7 @@ def find_my_solution (rectangles):
 
 
     print(widths,height)
-
+    '''
 
 
 #-------------------ID The Boxes-------------------------------------
@@ -56,18 +58,28 @@ def find_my_solution (rectangles):
         temp = id(rectangles[i])
         initial_placement.append((temp,i))
 #-------------------Sort The Boxes-----------------------------------
+    
     #Make a new list of sorted boxes
     sorted_rectangles = sortByArea(rectangles)
 
-
-    for box in sorted_rectangles:
+    for box in rectangles:
         width = box[0]
+        height = box[1]
+
         coordinate = (upper_left_x, upper_left_y)   # make a tuple
         placement.append(coordinate)             # insert tuple at back of list
         upper_left_x = upper_left_x + width
 
+
     return placement
 
+#This function sorts the rectangles by width. It takes
+#a list of tuples, unsorted, and returns those boxes
+#sorted by width. Greatest -> smallest
+def sortByWidth(rectangles):
+    sorted_by_width = sorted(rectangles)
+    sorted_by_width.reverse()
+    return sorted_by_width
 
 #These two functions sort the tuples based on area (w*h)
 def f(R):
@@ -75,19 +87,61 @@ def f(R):
 def sortByArea(rectangles):
 	return sorted(rectangles,key=f,reverse=True)
 
+#---------------Sort Back Function-------------------
+#This function builds a final placement list, from a
+#sorted list of boxes based on uniqueID. The final placement
+#returns the boxes in their original order. The function
+#takes in the initial_placement list from the ID function,
+#the list of sorted rectangles, and returns the placement
+#of the boxes based on their unique ID's
+def sortBack(initial_placement, sorted_rectangles):
+    final_placement = []
+    length = len(initial_placement)
+    for i in range(length):#Loop through initial placement
+        #Grab the ID if the first Box
+        initBox = initial_placement[i]
+        initBoxID = initBox[0]
+        for j in range(length):#Loop through the sorted boxes
+            #Grab the ID of the sorted box
+            sortBox = sorted_rectangles[j]
+            boxID = id(sortBox)
+            if(initBoxID==boxID):
+                tempTup = initial_placement[j]
+                posIndex = tempTup[1]
+                final_placement.insert(i,sortBox)
+    return final_placement
+
+
+
+
+
+
+
 
 #Function gets called
 def find_solution(rectangles):
     return find_my_solution(rectangles)  # a working example!
 
+'''
+Here is an outline of the program:
+- Take in rectangles (done)
+-ID the Rectangles (done)
+-sort the Rects. Here we can do this by either area or width.
+  width makes more sense.  (done)
+- Stack the Rects (while loop or called recursively)
+   1. Function to update the skyline
+   2. Function to Find where to place
+      (If Sj-1 > Sj => Leftmost
+       If Sj > Sj-1 => Rightmost
+       Left Endpoint
+       Right Endpoint)
+    3. Function to Place the Rect. (Return updated placement list)
+       This is a list of positions of the rect and NOT the (w,l)
+       The trick here is to either place it in its correct Index based
+       on its Unique ID, OR to have someway to identify which box this is
+       A. Note I have written a function to sort Back the list based on Unique ID (done)
 
-
-
-
-
-
-
-
+'''
 
 
 
