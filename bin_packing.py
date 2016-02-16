0cf821e53a1c789a496fe63db6c75229ee771a4e
folkpark <parkerfolkman@gmail.com>
2016-02-15 20:08:32 -0700

    Update bin_packing.py

--- bin_packing.py
+++ bin_packing.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------
 # CSCI 338, Spring 2016, Bin Packing Assignment
 # Author: Parker Folkman
-# Last Modified: January 31, 2016
+# Last Modified: February 15, 2016
 # ----------------------------------------------
 #
 #
@@ -29,45 +29,45 @@ from operator import mul
 def find_my_solution (rectangles):
     length = len(rectangles)
     placement = []
+    initial_placement = []
     upper_left_x = 0
     upper_left_y = 0
 
-    widths = [0]
-    height = [1]
-
-
-
-
-    '''
-    #Fill the array with X's
-    for i in range(length): #Length
-        tempBox = rectangles[i]
-        widths.append(tempBox)
-        height.append(tempBox)
-
-
-    print(widths,height)
-    '''
-
-
-#-------------------ID The Boxes-------------------------------------
     #ID the boxes. Stores in new list
     #Stores in format: (Unique ID, index location in list)
-    initial_placement = []
     for i in range(length):
         temp = id(rectangles[i])
         initial_placement.append((temp,i))
-#-------------------Sort The Boxes-----------------------------------
-
+        
     #Make a new list of sorted boxes
-    sorted_rectangles = sortByArea(rectangles)
+    sorted_rectangles = sortByWidth(rectangles)
 
-    for box in rectangles:
+    print(rectangles)
+    print(sorted_rectangles)
+    
+    for i in range(len(sorted_rectangles)):
+        box = sorted_rectangles[i]
         width = box[0]
-        coordinate = (upper_left_x, upper_left_y)   # make a tuple
-        placement.append(coordinate)             # insert tuple at back of list
+        height = box[1]
+        boxID = id(box)
+        index = getIndex(boxID, initial_placement)
+        print(index)
+        coordinate = (upper_left_x, upper_left_y,index) #make a tuple
+        placement.insert(index, coordinate)
         upper_left_x = upper_left_x + width
+
+    placement = sortBack(placement)
+    print(placement)
     return placement
+#---------End_Find_my_Solution------------------------------------------
+
+def getIndex(ID, initial_placement):
+    for i in range(len(initial_placement)):
+        tempRect = initial_placement[i] #Temporary rectangle
+        tempID = tempRect[0] #temporary ID of that rect
+        if(tempID==ID):
+            index = tempRect[1]
+            return index
 
 #This function sorts the rectangles by width. It takes
 #a list of tuples, unsorted, and returns those boxes
@@ -77,12 +77,38 @@ def sortByWidth(rectangles):
     sorted_by_width.reverse()
     return sorted_by_width
 
+#This function sorts the rectangles by height. It takes
+#a list of tuples, unsorted, and returns those boxes
+#sorted by height. Greatest -> smallest
+def sortByHeight(rectangles):
+    sorted_by_height = sorted(rectangles,key=lambda x: x[1])
+    sorted_by_height.reverse()
+    return sorted_by_height
+
 #These two functions sort the tuples based on area (w*h)
 def f(R):
     return R[0]*R[1]
 def sortByArea(rectangles):
 	return sorted(rectangles,key=f,reverse=True)
 
+def sortBack(placement):
+    newPlacement = []
+    for i in range(len(placement)):
+        tempCord = placement[i]
+        tempX = tempCord[0]
+        tempY = tempCord[1]
+        newCord = (tempX,tempY)
+        tempIndex = tempCord[2]
+        newPlacement.insert(tempIndex, newCord)
+    return newPlacement
+
+
+
+
+
+    
+    
+'''
 #---------------Sort Back Function-------------------
 #This function builds a final placement list, from a
 #sorted list of boxes based on uniqueID. The final placement
@@ -106,7 +132,7 @@ def sortBack(initial_placement, sorted_rectangles):
                 posIndex = tempTup[1]
                 final_placement.insert(i,sortBox)
     return final_placement
-
+'''
 
 
 
@@ -129,7 +155,6 @@ Here is an outline of the program:
        Once a box is placed, the skyline needs to be updated. This is done in two steps:
 	   1. Instantiates a new line segment corresponding to the top edge of b
 	   2. and updates existing segments effected
-
    2. Function to Find where to place
       (If Sj-1 > Sj => Leftmost
        If Sj > Sj-1 => Rightmost
@@ -142,7 +167,6 @@ Here is an outline of the program:
        A. Note I have written a function to sort Back the list based on Unique ID (done)
     4. Maintain a list of tuples of the placements of the Rect's in order
 - Return final_placement
-
 Other Needs:
 -Need to either pick a really reasonable fix size bin width (let height be dynamic) to start with, or
 make the bin capable of being dynamically sized
